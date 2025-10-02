82af58a8a857452f1dffbe6fbcf16273aa74ad03
itsuki <itsuki.enjyoy@gmail.com>
2025-10-02 11:23:09 +0900

    Initial Commit

---
+++ CustomFontProviderDemo/CustomFontProviderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontProviderDemoApp.swift
+//  CustomFontProviderDemo
+//
+//  Created by Itsuki on 2025/10/02.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontProviderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
